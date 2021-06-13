1cc594e573dca6e0ecc6b7f896d7585d38b77636
Ivana <stankovic.ivana1998@gmail.com>
2021-06-13 18:27:22 +0200

    Initial commit

---
+++ src/test/java/rs/bg/ac/student/ivana/MavenCommon/domain/DomainTypeTest.java
@@ -0,0 +1,49 @@
+package rs.bg.ac.student.ivana.MavenCommon.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DomainTypeTest {
+
+	@Test
+	void testGetTableName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetColumnNamesForInsert() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetInsertValues() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetJoinCondition() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetUpdateString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetRS() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetDeleteString() {
+		fail("Not yet implemented");
+	}
+
+}
